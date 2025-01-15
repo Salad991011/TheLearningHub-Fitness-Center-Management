51c71cd7d55d774b5471aced1acee958fa242236
Salad991011 <zeussrikethunder@gmail.com>
2025-01-16 02:15:27 +0300

    Changed Almost everything home page to be dynamic

--- TheLearningHub-Fitness Center Management/Controllers/HomeController.cs
+++ TheLearningHub-Fitness Center Management/Controllers/HomeController.cs
@@ -7,15 +7,57 @@ namespace TheLearningHub_Fitness_Center_Management.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ModelContext _context;    
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ModelContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
+           
         {
+
+            ViewBag.BackgroundImagePath1 = _context.HomePageContents.Select(x => x.BackgroundImagePath1).FirstOrDefault();
+            var backgroundImage = _context.HomePageContents
+          .Select(x => x.BackgroundImagePath1)
+          .FirstOrDefault();
+
+            ViewBag.BackgroundImagePath2 = _context.HomePageContents.Select(x => x.BackgroundImagePath2).FirstOrDefault();
+            var backgroundImage2 = _context.HomePageContents
+          .Select(x => x.BackgroundImagePath2)
+          .FirstOrDefault();
+
+            ViewBag.ServiceImagePath2 = _context.HomePageContents.Select(x => x.ServicesImagePath2).FirstOrDefault();
+            var ServiceImagePath2 = _context.HomePageContents
+          .Select(x => x.ServicesImagePath2)
+          .FirstOrDefault();
+
+            ViewBag.BACKGROUND_TITLE1 = _context.HomePageContents.Select(x => x.BackgroundTitle1).FirstOrDefault();
+            ViewBag.BACKGROUND_DESC1 = _context.HomePageContents.Select(x => x.BackgroundDesc1).FirstOrDefault();
+            ViewBag.COURSEL_TEXT_TITLE = _context.HomePageContents.Select(x => x.CourselTextTitle).FirstOrDefault();
+            ViewBag.COURSEL_DESC = _context.HomePageContents.Select(x => x.CourselDesc).FirstOrDefault();
+            ViewBag.COURSEL_ITEMS_TITLE = _context.HomePageContents.Select(x => x.CourselItemsTitle).FirstOrDefault();
+            ViewBag.COURSEL_ITEMS_DESC = _context.HomePageContents.Select(x => x.CourselItemsDesc).FirstOrDefault();
+        
+            ViewBag.BACKGROUND_TITLE2 = _context.HomePageContents.Select(x => x.BackgroundTitle2).FirstOrDefault();
+            ViewBag.BACKGROUND_DESC2 = _context.HomePageContents.Select(x => x.BackgroundDesc2).FirstOrDefault();
+            ViewBag.TRAINER_TITLE = _context.HomePageContents.Select(x => x.TrainerTitle).FirstOrDefault();
+            ViewBag.TRAINER_DESC = _context.HomePageContents.Select(x => x.TrainerDesc).FirstOrDefault();
+            ViewBag.SERVICES_TITLE = _context.HomePageContents.Select(x => x.ServicesTitle).FirstOrDefault();
+            ViewBag.SERVICES_DESC1 = _context.HomePageContents.Select(x => x.ServicesDesc1).FirstOrDefault();
+            ViewBag.SERVICES_DESC2 = _context.HomePageContents.Select(x => x.ServicesDesc2).FirstOrDefault();
+            // Handle if the database value is null or empty
+
+            ViewBag.BackgroundImagePath1 = string.IsNullOrEmpty(backgroundImage) ? "default.jpg" : backgroundImage;
+
+            ViewBag.BackgroundImagePath2 = string.IsNullOrEmpty(backgroundImage2) ? "default.jpg" : backgroundImage2;
+            
+            ViewBag.ServiceImagePath2 = string.IsNullOrEmpty(ServiceImagePath2) ? "default.jpg" : ServiceImagePath2;
+
+
             return View();
         }
 
